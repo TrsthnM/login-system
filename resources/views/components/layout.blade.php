44d44eee5c5aee02c30ca9944de4d1f4a9f1a072
TrsthnM <tristhandavem27@gmail.com>
2026-03-31 00:01:15 +0800

    Update dashboard and layout making it responsive

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,17 +7,27 @@
     @vite(['resources/css/app.css'])
     <title>Login & Register</title>
 </head>
-<body class="bg-linear-to-br from-white to-blue-200 h-screen">
-    <nav class="flex justify-between items-center py-4 px-90">
-        <h1 class="font-extrabold text-gray-800 text-3xl cursor-default">Trishan</h1>
-        <div>
-            <ul class="flex space-x-10">
-                <li><a href="/" class="font-semibold text-gray-800 text-xl tracking-wide">Dashboard</a></li>
-                <li><a href="/" class="font-semibold text-gray-800 text-xl tracking-wide">Log in</a></li>
+<body class="bg-linear-to-b from-white to-blue-200 lg:bg-linear-to-br lg:from-white lg:to-blue-200 h-screen">
+    <nav class="flex justify-between items-center bg-white lg:bg-transparent py-4 px-4 md:px-30 xl:px-90 relative">
+        <h1 class="font-extrabold text-gray-800 text-2xl lg:text-3xl cursor-default">Tristhan</h1>
+        <div class="lg:hidden">
+            <input type="checkbox" id="menu" class="hidden peer"/>
+            <label for="menu" class="cursor-pointer block p-2 font-bold">
+                <span class="block w-6 h-0.5 bg-gray-800 mb-1"></span>
+                <span class="block w-6 h-0.5 bg-gray-800 mb-1"></span>
+                <span class="block w-6 h-0.5 bg-gray-800"></span>
+            </label>
+            <ul class="flex flex-col items-center justify-center absolute top-full left-0 w-full h-screen bg-linear-to-b from-white to-blue-200 space-y-5 max-h-0 overflow-hidden peer-checked:max-h-150">
+                <li><a href="/" class="font-semibold text-gray-800 text-xl">Dashboard</a></li>
+                <li><a href="/" class="font-semibold text-gray-800 text-xl">Log in</a></li>
             </ul>
         </div>
+        <div class="hidden lg:flex space-x-10">
+            <a href="/" class="font-semibold text-gray-800 text-md lg:text-xl tracking-wide">Dashboard</a>
+            <a href="/" class="font-semibold text-gray-800 text-md lg:text-xl tracking-wide">Log in</a>
+        </div>
     </nav>
-    <main class="px-90">
+    <main class="px-4 md:px-30 xl:px-90">
         {{ $slot }}
     </main>
 </body>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-    <div class="mt-75">
-        <h1 class="font-bold text-4xl text-gray-800 mb-10">Welcome to my Login & Register System</h1>
-        <p>This simple login and register system helps you secure your website and manage user access easily.
+    <div class="mt-30 lg:mt-65">
+        <h1 class="font-bold text-xl lg:text-4xl text-gray-800 mb-10">Welcome to my Login & Register System</h1>
+        <p class="text-sm lg:text-lg text-justify">This simple login and register system helps you secure your website and manage user access easily.
             It ensures that only authorized users can enter your system and protects important data from unauthorized access.</p>
         <div class="mt-15">
             <a href="/" class="bg-gray-800 text-white font-bold py-3 px-10 rounded-2xl">Register</a>
