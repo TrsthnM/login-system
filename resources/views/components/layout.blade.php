a2ac4585834b68db1ee5246e2a511c2e7917ae38
TrsthnM <tristhandavem27@gmail.com>
2026-03-30 21:00:53 +0800

    Update layout adding nav and bg

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,17 @@
     @vite(['resources/css/app.css'])
     <title>Login & Register</title>
 </head>
-<body>
+<body class="bg-linear-to-br from-white to-blue-200 h-screen">
+    <nav class="bg-white flex justify-between items-center py-4 px-90 border-b border-b-gray-300">
+        <h1 class="font-extrabold text-gray-800 text-3xl cursor-default">Trishan</h1>
+        <div>
+            <ul class="flex space-x-10">
+                <li><a href="/" class="font-semibold text-gray-800 text-xl">Dashboard</a></li>
+                <li><a href="/" class="font-semibold text-gray-800 text-xl">Log in</a></li>
+                <li><a href="/" class="font-semibold text-gray-800 text-xl">Register</a></li>
+            </ul>
+        </div>
+    </nav>
     {{ $slot }}
 </body>
 </html>
